531997314e9c88adf523e092d1a8c7e01c666349
vishal-gawade <vishalgawade.se@gmail.com>
2024-01-02 23:27:25 +0530

    solution for Valid Anagram problem

---
+++ NeetCode150/Valid Anagram.py
@@ -0,0 +1,36 @@
+# Brute force approach TC O(n) SC O(n)
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True
+    
+#
+
+# TC O(nlogn) SC O(n)
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        return sorted(s) == sorted(t)
+
+            
