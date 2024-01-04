f1df67697a8330011be263206ed903c519533cd2
vishal-gawade <vishalgawade.se@gmail.com>
2024-01-04 23:27:41 +0530

    Adding Anagram solution

---
+++ NeetCode150/Group Anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = defaultdict(list) # mapping char count to list of anagrams
+        
+        for s in strs:
+            count =  [0] * 26 
+            for c in s:
+                count[ord(c)-ord("a")] =+ 1
+            res[tuple(count)].append(s) # list cannot be keys in Dictionary
+        return res.values()
--- NeetCode150/notes.py
+++ NeetCode150/notes.py
@@ -1,4 +1,5 @@
 
+# Valid Anagram 
 class Anagram(object):
     def anagramm(self, s, t):
         self.s = s
@@ -9,9 +10,12 @@ class Anagram(object):
         countS, countT = {}, {}
         
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-        print(countS.get(s[i]))
+            countS[s[i]] = 1 + countS.get(s[i],0) #if key is not present
+        print(countS.get(s[i])) 
         print(s[i])
     
 anagram = Anagram()
 anagram.anagramm('anagram', 'nagaram')
+
+#Group Anagram
+
